2c54cb8b321eebcfceae69b3e16f76dbdf4715f4
alexandredeleze <alexandre.deleze@epfl.ch>
2020-09-03 09:44:38 +0200

    Refactor

--- pryv.io/cluster/scripts/renew-certif.sh
+++ pryv.io/cluster/scripts/renew-certif.sh
@@ -7,12 +7,8 @@ LEADER="http://0.0.0.0:7000"
 USERNAME="admin"
 PASSWORD="admin"
 log="/var/log/renew_cert.log"
-set -e
-if (([ ! -f $LETSENCRYPT_DIR/$DOMAIN/fullchain.pem ] || [ ! -f $LETSENCRYPT_DIR/$DOMAIN/privkey.pem ]) || \
-([ $(echo "$((($(echo | openssl x509 -enddate -noout -in $LETSENCRYPT_DIR/$DOMAIN/fullchain.pem| sed 's/^.\{9\}//' | date  -f - '+%s') - $(date '+%s'))/43200))") -lt 30 ] && \
-[ $(echo "$((($(echo | openssl x509 -enddate -noout -in $LETSENCRYPT_DIR/$DOMAIN/privkey.pem| sed 's/^.\{9\}//' | date  -f - '+%s') - $(date '+%s'))/43200))") -lt 30 ])); then
-    echo $DOMAIN
-    cp -rf $LETSENCRYPT_DIR/$DOMAIN $LETSENCRYPT_DIR/tmp/$DOMAIN
+
+function installNecessaryDependencies() {
     apt-get update
     pkgs='certbot'
     if ! dpkg -s $pkgs >/dev/null 2>&1; then
@@ -34,18 +30,18 @@ if (([ ! -f $LETSENCRYPT_DIR/$DOMAIN/fullchain.pem ] || [ ! -f $LETSENCRYPT_DIR/
     if [ `npm list | grep -c $package` -eq 0 ]; then
         npm install $package --no-shrinkwrap
     fi
+}
 
+function requestCertificate() {
     {
         echo "Y" | certbot certonly --manual --manual-auth-hook $SCRIPTS/hook.js -d *.$DOMAIN --dry-run
     } || {
         echo $(date) Error to create new certificate >> $log
         }
+}
 
-    date=`date "+%Y%m%d"`
-    dateFull=`echo | openssl x509 -enddate -noout -in $LETSENCRYPT_DIR/$DOMAIN/fullchain.pem| sed 's/^.\{9\}//' | date  -f - "+%Y%m%d"` || echo $(date) Error: $LETSENCRYPT_DIR/$DOMAIN/cert.pem does not exist >> $log
-    datePriv=`date -r "$LETSENCRYPT_DIR/$DOMAIN/privkey.pem" "+%Y%m%d"`|| echo $(date) Error: $LETSENCRYPT_DIR/$DOMAIN/privkey.pem does not exist >> $log
-    if ([ -f "$LETSENCRYPT_DIR/$DOMAIN/fullchain.pem" ] && [ "$date" == "$dateFull" ] && [ -f "$LETSENCRYPT_DIR/$DOMAIN/privkey.pem" ] && [ "$date" == "$datePriv" ]); then
-        directories=`find $DATA -name "secret" -type d`
+function propagateCertificate() {
+    directories=`find $DATA -name "secret" -type d`
         echo "$directories" | while read directory; do
             # When acknowledged, put fullchain.pem > pryv.li-bundle.crt and privkey.pem > pryv.li-key.pem
             cp $LETSENCRYPT_DIR/$DOMAIN/test_renew.crt $directory/test.crt
@@ -55,8 +51,10 @@ if (([ ! -f $LETSENCRYPT_DIR/$DOMAIN/fullchain.pem ] || [ ! -f $LETSENCRYPT_DIR/
         token=`curl -s -X POST -H "Content-Type: application/json" -d "{\"username\": \"$USERNAME\", \"password\": \"$PASSWORD\"}" $LEADER/auth/login | \
         jq '.token' | tr -d '"'`
         curl -X POST -H "Authorization: $token" $LEADER/admin/notify
+}
 
-        followers=`jq '.followers[].url' /app/conf/config-leader.json`
+function checkCertificateFollowers() {
+    followers=`jq '.followers[].url' /app/conf/config-leader.json`
         echo "$followers" | while read follower; do
             follower=`tr -d '"' <<< "$follower"`
             if [[ $follower == https://* ]];then
@@ -69,6 +67,22 @@ if (([ ! -f $LETSENCRYPT_DIR/$DOMAIN/fullchain.pem ] || [ ! -f $LETSENCRYPT_DIR/
                 fi
             fi
         done
+}
+
+set -e
+
+if (([ ! -f $LETSENCRYPT_DIR/$DOMAIN/fullchain.pem ] || [ ! -f $LETSENCRYPT_DIR/$DOMAIN/privkey.pem ]) || \
+([ $(echo "$((($(echo | openssl x509 -enddate -noout -in $LETSENCRYPT_DIR/$DOMAIN/fullchain.pem| sed 's/^.\{9\}//' | date  -f - '+%s') - $(date '+%s'))/43200))") -lt 30 ])); then
+    cp -rf $LETSENCRYPT_DIR/$DOMAIN $LETSENCRYPT_DIR/tmp/$DOMAIN
+    installNecessaryDependencies
+    requestCertificate
+
+    date=`date "+%Y%m%d"`
+    dateFull=`echo | openssl x509 -enddate -noout -in $LETSENCRYPT_DIR/$DOMAIN/fullchain.pem| sed 's/^.\{9\}//' | date  -f - "+%Y%m%d"` || echo $(date) Error: $LETSENCRYPT_DIR/$DOMAIN/cert.pem does not exist >> $log
+    datePriv=`date -r "$LETSENCRYPT_DIR/$DOMAIN/privkey.pem" "+%Y%m%d"`|| echo $(date) Error: $LETSENCRYPT_DIR/$DOMAIN/privkey.pem does not exist >> $log
+    if ([ -f "$LETSENCRYPT_DIR/$DOMAIN/fullchain.pem" ] && [ "$date" == "$dateFull" ] && [ -f "$LETSENCRYPT_DIR/$DOMAIN/privkey.pem" ] && [ "$date" == "$datePriv" ]); then
+        propagateCertificate
+        checkCertificateFollowers
 
     else
         rm -rf $LETSENCRYPT_DIR/$DOMAIN
