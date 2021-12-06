6a0723bcfec2922de612266329338a567a58690c
Ilia Kebets <kebetsi@gmail.com>
2021-12-06 13:36:36 +0100

    print name of file where we write

--- pryv.io/single-node/get-services-logs.sh
+++ pryv.io/single-node/get-services-logs.sh
@@ -6,5 +6,5 @@ export PRYV_CONF_ROOT="/var/pryv"
 # log provided number of lines or 500
 lines="${1:-500}"
 
-echo "Fetching Pryv logs"
+echo "Fetching Pryv logs to pryv-services.log"
 docker-compose -f pryv/pryv.yml logs --tail=$lines > pryv-services.log
